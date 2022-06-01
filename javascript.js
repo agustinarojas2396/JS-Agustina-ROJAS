90b2419ff8a224ca415d30b2f9479bfbf971031e
agusrojas <agussrojas.00@gmail.com>
2022-06-01 17:49:17 -0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,18 @@
+let eleccion = parseInt( prompt('Enumere del 1 al 10 como está el té'));
+
+for (let i=0; i <=10 ; i++) {
+    if(eleccion == 1) alert ('Para nada rico')
+        else if (eleccion == 2 ) alert('safable')
+        else if ( eleccion == 3) alert('un poco rico')
+        else if ( eleccion == 4) alert('Mas o menos rico')
+        else if ( eleccion == 5) alert('rico')
+        else if ( eleccion == 6) alert('algo rico')
+        else if ( eleccion == 7) alert('bastante rico')
+        else if ( eleccion == 8) alert('muy rico')
+        else if ( eleccion == 9) alert('riquisimo')
+        else if ( eleccion == 10) alert('perfecto')
+
+
+
+    console.log(i)
+}
